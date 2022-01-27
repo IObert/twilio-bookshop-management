199aac7647a3827eb7a7c9c72ab950efc4a03027
Marius Obert <obatzer@gmail.com>
2022-01-27 10:56:05 +0100

    Trigger an warning SMS on low supply

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,4 +1,5 @@
 const cds = require("@sap/cds");
+const twilioClient = require("twilio")();
 
 class CatalogService extends cds.ApplicationService {
   init() {
@@ -7,11 +8,23 @@ class CatalogService extends cds.ApplicationService {
     // Reduce stock of ordered books if available stock suffices
     this.on("submitOrder", async (req) => {
       const { book, quantity } = req.data;
-      let { stock } = await SELECT`stock`.from(Books, book);
-      if (quantity > stock) {
+      let { stock, title } = await SELECT`stock, title`.from(Books, book);
+      const remaining = stock - quantity;
+      if (remaining < 0) {
         return req.reject(409, `${quantity} exceeds stock for book #${book}`);
       }
-      await UPDATE(Books, book).with({ stock: (stock -= quantity) });
+      if (remaining < 10) {
+        console.log(`A customer just ordered ${stock}x "${title}".`);
+        twilioClient.messages
+          .create({
+            body: `A customer just ordered ${stock}x "${title}" and there are only ${remaining} left in stock.`,
+            from: process.env.TWILIO_SENDER,
+            to: process.env.TWILIO_RECEIVER,
+          })
+          .then((message) => console.log(message.sid))
+          .catch((message) => console.error(message));
+      }
+      await UPDATE(Books, book).with({ stock: remaining });
       await this.emit("OrderedBook", { book, quantity, buyer: req.user.id });
       return { stock };
     });
